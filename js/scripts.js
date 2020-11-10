689784df77d97ab38d7c40e24eeb343b35ff22c8
Justin Mello <justinmello32@hotmail.com>
2020-11-10 10:23:09 -0800

    Decent working copy of js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 		$(document).ready(function(){
 			
 			//Initial Variables
+			var winAmount = 0;
 			var winnings = localStorage.getItem("total_winnings");
 			var betAmount= 0;
 			localStorage.setItem("total_winnings", 0)
@@ -11,17 +12,38 @@
   				"cherry"
 			];
 			
-
-
 			//Event Listener
 			$("#spin-button").on("click",function(){
 				spinWheel();
-			});//click
-
-
+			}); //Click Listener
 
 			//Functions Here
 			function spinWheel() {
+
+               var slotMachineImage1 = Math.floor(Math.random()  * slotImages.length);
+               var slotMachineImage2 = Math.floor(Math.random()  * slotImages.length);
+               var slotMachineImage3 = Math.floor(Math.random()  * slotImages.length);
+
+               $("#position1").attr("src", `img/${slotMachineImage1}.png`);
+               $("#position2").attr("src", `img/${slotMachineImage2}.png`);
+               $("#position3").attr("src", `img/${slotMachineImage3}.png`);
+
+               //All images are different than each other, x3 bet amount
+               if(slotMachineImage1 != slotMachineImage2 && slotMachineImage1 != slotMachineImage3 && 
+               	slotMachineImage2 != slotMachineImage3) {
+                    console.log("All items are different, you lose!");
+               }
+
+               //All images match each other, x5 bet amount
+               if(slotMachineImage1 == slotMachineImage2 && slotMachineImage1 == slotMachineImage3) {
+         			console.log("All items match!");
+               }
+               //There are two items that match 
+               else if(slotMachineImage1 == slotMachineImage2 || slotMachineImage1 == slotMachineImage3 || 
+               	slotMachineImage2 == slotMachineImage3) {
+               		console.log("Two items match!");
+               }
+
 				betAmount = $("#bet-amount").val();
 				winnings = +winnings + +betAmount;
 				localStorage.setItem("total_winnings", betAmount);
