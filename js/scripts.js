83af13252ceda401d3b23662d3fc7586208c56b6
Justin Mello <justinmello32@hotmail.com>
2020-11-09 20:42:35 -0800

    Initial Commit.

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+		$(document).ready(function(){
+			$("#spin-button").on("click",function(){
+				console.log($("#bet-amount").val());
+			});//click
+		})//ready
+	
---
+++ scripts.js
@@ -0,0 +1,9 @@
+	<script>
+		$(document).ready(function(){
+
+			$("button").on("click",function(){
+
+				console.log($("Hi there");
+			})//click
+		})//ready
+	</script>
