90f0e2b362780cdd38ae0810d62da28be80ef93f
Justin Mello <justinmello32@hotmail.com>
2020-11-09 21:59:26 -0800

    Adding logic and images.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
 		$(document).ready(function(){
+			
+			//Initial Variables
+			var winnings = localStorage.getItem("total_winnings");
+			var betAmount= 0;
+			localStorage.setItem("total_winnings", 0)
+			
+			//Event Listener
 			$("#spin-button").on("click",function(){
-				console.log($("#bet-amount").val());
+				spinWheel();
 			});//click
+
+
+
+			//Functions Here
+			function spinWheel() {
+				betAmount = $("#bet-amount").val();
+				winnings = +winnings + +betAmount;
+				localStorage.setItem("total_winnings", betAmount);
+				$("#totalWinnings").html(`Total Winnings: ${winnings}`);
+		}
+
+
+
 		})//ready
 	
